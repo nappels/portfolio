b0bad35576f0ec0cfb01466a5342ce45247fe455
Noah Appel <nappel@aquto.com>
2014-01-12 22:09:43 -0500

    Skills and contact sections

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -6,23 +6,22 @@
 
 		// Mobile Nav IIFE
 		(function() {
-			
+
 			var isModalOpen = false;
 
 			var openModal = function() {
 				$('.wrapper').animate({
-						'left': '-300px'
-					},600);
+						'left': '-235px'
+					},400);
 					// Set toggle to true
 					isModalOpen = true;
 					// Bind a click handler to the wrapper to close the click handler
 					$('.wrapper').on('click', function(){ closeModal(); })
 			};
 			var closeModal = function() {
-				console.log('closeModal');
 				$('.wrapper').animate({
 						'left': '0'
-					},600);
+					},400);
 					// Set toggle key to false
 					isModalOpen = false;
 					// Unbind the click on the wrapper to save memory
@@ -40,6 +39,33 @@
 			};
 
 			$('.hamburger').stop().click(mobileNavToggle);
+		})();
+
+		// Gravatar IIFE
+		(function() {
+			var md5Email = 'aec6e5136a0c26f669ba569e72a153a8';
+			var size = 150;
+
+			$('.gravatar').attr('src','http://www.gravatar.com/avatar/' + md5Email + '.jpg?s=' + size)
+		})();
+
+		// Nav Scroll IIFE
+		(function() {
+			var lastScrollTop = 0;
+			$(window).scroll(function() {
+				var scrollTop = $(this).scrollTop();
+				if (scrollTop > lastScrollTop && $(this).width() > 851) {
+					// Scroll Down
+					if (scrollTop > 100) {
+						$('.header').slideUp(300);
+					}
+				} 
+				else {
+					// Scroll Up
+					$('.header').slideDown(300);
+				}
+				lastScrollTop = scrollTop;
+			});
 		})();
 	});
 
